21a810b06855db2e8ab5e41de9f2cb8050afc2b7
Nicolas <nick_.s@hotmail.com>
2020-09-18 12:07:54 -0300

    Tamplate dotnet command for terminal

---
+++ auladotnet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace auladotnet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
