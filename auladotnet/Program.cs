62a4ad539316c07b3974349f0516c3ca41d523d6
Nicolas <nick_.s@hotmail.com>
2020-09-18 13:00:36 -0300

    Created loop FOR in Hello World

--- auladotnet/Program.cs
+++ auladotnet/Program.cs
@@ -5,8 +5,14 @@ namespace auladotnet
     class Program
     {
         static void Main(string[] args)
+                   
         {
-            Console.WriteLine("Hello World!");
+            int numeroDeVezes = 10;
+             
+            for (int i=0; i < numeroDeVezes; i++)
+        
+            Console.WriteLine($"Hello World! {i} ");
+                    
         }
     }
 }
